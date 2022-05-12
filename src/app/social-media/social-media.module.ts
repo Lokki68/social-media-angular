583aa9a9233d1e52c50ef662cf2d0c6682975221
Brice <bricel67@gmail.com>
2022-05-12 14:33:13 +0200

    feat - app

---
+++ src/app/social-media/social-media.module.ts
@@ -0,0 +1,14 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SocialMediaRoutingModule } from './social-media-routing.module';
+
+
+@NgModule({
+  declarations: [],
+  imports: [
+    CommonModule,
+    SocialMediaRoutingModule
+  ]
+})
+export class SocialMediaModule { }
