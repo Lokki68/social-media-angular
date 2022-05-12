fe138d337f933471ad368f61cfcf53a590053093
Brice <bricel67@gmail.com>
2022-05-12 14:32:49 +0200

    feat - app

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {SocialMediaModule} from "./social-media/social-media.module";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: 'social-media', loadChildren: () => import('./social-media/social-media.module').then(m => m.SocialMediaModule)},
+  {path:'**', redirectTo: 'social-media' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
