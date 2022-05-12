be19bf356af84261caf512e86a7016a2dde493f5
Brice <bricel67@gmail.com>
2022-05-12 14:43:33 +0200

    feat - header

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,6 +2,7 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './components/header/header.component';
 import {SharedModule} from "../shared/shared.module";
+import {RouterModule} from "@angular/router";
 
 
 
@@ -11,7 +12,8 @@ import {SharedModule} from "../shared/shared.module";
   ],
   imports: [
     CommonModule,
-    SharedModule
+    SharedModule,
+    RouterModule
   ],
   exports: [
     HeaderComponent
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import {MatToolbarModule} from "@angular/material/toolbar";
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    MatToolbarModule
   ]
 })
 export class SharedModule { }
